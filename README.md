6386506141f5ce6b0dbf7970f599c63c42fbb923
Joaquin Fernandez <copako12@gmail.com>
2020-03-06 23:16:31 -0300

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# calculator
+# Calculator
+
+Used vanilla Javascript, html and css
+
+## This was my calculator project for TheOdinProject curriculum.
+
+
+### History
+
+This is not the first attempt or the first one, my previous calculator had a millon bugs, the code was unredeable
+and i was lost in my own words, because of that i decided to start focusing on made my code clean, as well to think in a more practical
+way to do this calculator, so i watch a lot of videos, and there was a video that got my attention, it was totally clean
+and written in a way i'd understanded everything, so i start my calculator with that version of a calculator in my head.
+
+
+It bassically treats the calculator as an object with different methods, this methods are the calculator functions, and each function
+correleates to each other function, some methods are purely to do simple things, like add a number, other are more complex, for example,
+compute the result.
+
+Basically i learn to treat objects in Javascript as real objects, but not forgetting about the most simplest functionalities and also remember
+to create the 'memory' of the object, and if the object has a 'display' or something like that, update everytime needed.
+
+### Summary
+
+The order of the project (script) was the next one: 
+-first select all elements from the HTML file that i was going to use
+-create the class Object and pre-think the methods, and also create the contstructor
+-start from the basics methods step by step, like displaying a number, store it somewhere, and if the person put more numbers, concatenete them.
+-create eventListener from every input that the user can do
+-this eventListeners will, simply, call the methods from the calculator, and if needed, give them a value to use (numbers example, when clicked, it runs a method with the argument being
+the text of the button(current number)).
+
+-and then, create the logic and connections between methods, being this a lot easiers because we have a skeleton to work, a skeleton to start using my braind
+, in the past i started doing logic and make eventListener do all the job, putting fucntions inside them...It could work, but it was nasty and messy.
+
+
+### Credits
+
+Most of the credits goes to : Web Dev Simplified
+I did actually know the fundamentals and follow a path to be able to understand the way he did it, so now i can continue writing cleaner code.
+
+### If you see the javascript file, there is a lot of comments in spanish, my native language, that is because i'm still learning and it helps me remember things better.
+
+
+
--- script.js
+++ script.js
@@ -1,36 +1,60 @@
-class Calculator {
+/*
+El pensamiento detrás de esta calculadora es crear una class Calculator que, aparte de crear un objecto calculadora,
+le otorgue metodos(funciones) especiales de una calculadora, estos son: mostrar valores en la pantalla, limpiarlos,
+actualizarlos cada vez que se cambie algo, borrar un caracter, escribir un numero, elegir una operacion, hacer las cuentas.
+
+Entonces al crear una nueva calculadora, esta tendrá todos los metodos designados.
+
+Luego hay que buscar los elementos del HTML para poder recibir eventos.
+
+A estos elementos html les asignamos eventListeners para que, al ser activados, activen el metodo indicado, o los metodos, eso si, 
+en cada event listener, aparte del metodo correspondiente, se le asigna el metodo de actualizar la pantalla, para que cada accion del usario se vea reflejada en pantalla.
+
+----------
+
+En la clase Calculadora, vamos a tener variables designadas para mostrarse y para poder calcular el resultados, estas son currentOperand y previousOperand.
+Un ejemplo de esto, es, al actualizar la pantalla, el valor almacenado en currentOperand pase a ser el innerText de el elemento designado a manejar la pantalla.
+
+
+
+(Esta cantidad de comentarios es debido a que estoy aprendiendo y quiero recordar todo y ordenarlo en mi cabeza :))
+*/
+
+
+
+class Calculator {                                                                  
     constructor(previousOperandTextElement,currentOperandTextElement){
         this.previousOperandTextElement = previousOperandTextElement;
         this.currentOperandTextElement = currentOperandTextElement;
         this.clear()
     }
-
+    //Metodo que limpia(resetea todos los valores)
     clear(){
-        this.currentOperand = '';  
-        this.previousOperand = '';
-        this.operation = undefined;
+        this.currentOperand = '';                                                   //currentOperand es el operand al cual podemos modificar con numeros o puntos,siempre que modificamos, es en este
+        this.previousOperand = '';                                                  //previousOperand guarda currentOperand luego de que elijamos operation
+        this.operation = undefined;                                                 //Se guarda el simbolo de operación elegido
     }
-
+    //Borra el ultimo digito agregado al currentOperand (slice(0,-1) borra el último =O)
     delete(){
-        this.currentOperand = this.currentOperand.toString().slice(0 , -1);
+        this.currentOperand = this.currentOperand.toString().slice(0 , -1);        //Vuelve currentOperand un string para poder cortarle el último caracter
     }
-
+    //Metodo que recibe un numero del eventlistener y lo pone en el currentOperand
     appendNumber(number){
         //No deja poner más de 1 punto                                              //Check in
         if(number === '.' && this.currentOperand.includes('.')) return;             
         this.currentOperand = this.currentOperand.toString() + number.toString();   //Agrega el numero en forma de string a el string ya establecido.
     }
 
-    chooseOperation(operation){
+    chooseOperation(operation){                                                     //Este método es el que almacena la operation designada desde el eventlistener
         //Evita que se ejecute si todavía no escribimos la 2da parte                //Check in
         if(this.currentOperand === '') return;                                      
-        //Si ya tenemos un previous, se ejecute compute, que realiza la operación
+        //Si ya tenemos un previousOperand, se ejecute compute, que realiza la operación
         if(this.previousOperand !== ''  ){
             this.compute();
         }                                                                       
-        this.operation = operation;                                               
-        this.previousOperand = this.currentOperand;
-        this.currentOperand = '';
+        this.operation = operation;                                                 //Esto pasa si no tenemos operation elegida aun
+        this.previousOperand = this.currentOperand;                                 //Previous toma el valor de current y pasa para arriba
+        this.currentOperand = '';                                                   //Ya almacenado previousOperand, podemos vaciar currentOperand para escribir la 2da parte de la cuenta
     }
 
     compute(){
@@ -71,11 +95,12 @@ class Calculator {
 
     updateDisplay(){
         this.currentOperandTextElement.innerText = this.currentOperand;         //Actualiza la pantalla (en forma de strings)
-        if(this.operation != null){
-            this.previousOperandTextElement.innerText = 
+        if(this.operation != null){                                             //De existir una operation
+            this.previousOperandTextElement.innerText =                         //Agregar el previousOperand y a su derecha el simbolo de la operación
                 `${this.previousOperand}${this.operation}`;
         }else {
-            this.previousOperandTextElement.innerText = '';
+            this.previousOperandTextElement.innerText = '';                     //Si no pasé un nuevo operation, simplemente se actualizo el valor de previous, borrandolo
+                                                                                //para poder luego darle un nuevo valor.
         }
     }
 }
@@ -125,9 +150,6 @@ equalsButton.addEventListener('click', () => {
     calculator.compute();
     calculator.updateDisplay();
 });
-
-
-
 
 deleteButton.addEventListener('click', () =>{
     calculator.delete();
